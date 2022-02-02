2655f3c0de710eb695e2ee539856e201da899e70
hgobr <hugo.brothier@hotmail.fr>
2022-02-02 20:51:54 +0100

    add transitions

--- js/script.js
+++ js/script.js
@@ -1,3 +1,31 @@
+// transitions
+
+window.onload = () => {
+  const transition = document.querySelector('.transition');
+  const anchors = document.querySelectorAll('a');
+
+  setTimeout(() => {
+    transition.classList.remove('isActive');
+  }, 500);
+
+  for (let i = 0; i < anchors.length; i++) {
+    const anchor = anchors[i];
+
+    anchor.addEventListener('click', (e) => {
+      e.preventDefault();
+      let target = e.target.href;
+
+      transition.classList.add('isActive');
+
+      setTimeout(() => {
+        window.location.href = target;
+      }, 500);
+    });
+  }
+};
+
+// notifications
+
 const notyf = new Notyf({
   duration: 2000,
   position: {
